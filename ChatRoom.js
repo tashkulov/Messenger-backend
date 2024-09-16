44baea67e866666b85fbaf6efead566c2abd4331
Nurislam <nurislamtashkulov168@gmail.com>
2024-09-16 13:30:59 +0600

    to colyseus back

--- ChatRoom.js
+++ ChatRoom.js
@@ -1,44 +1,44 @@
-// const { Room } = require('colyseus');
-//
-// class ChatRoom extends Room {
-//     onCreate(options) {
-//         this.messages = [];
-//
-//         this.onMessage("message", (client, message) => {
-//             const newMessage = { id: `${Date.now()}-${Math.random()}`, author: client.sessionId, ...message };
-//             this.messages.push(newMessage);
-//             this.broadcast("message", newMessage);
-//         });
-//
-//         this.onMessage("edit-message", (client, updatedMessage) => {
-//             const messageIndex = this.messages.findIndex(m => m.id === updatedMessage.id);
-//             if (messageIndex !== -1 && this.messages[messageIndex].author === client.sessionId) {
-//                 this.messages[messageIndex].text = updatedMessage.text;
-//                 this.broadcast("edit-message", this.messages[messageIndex]);
-//             }
-//         });
-//
-//         this.onMessage("delete-message", (client, { id }) => {
-//             const messageIndex = this.messages.findIndex(m => m.id === id);
-//             if (messageIndex !== -1 && this.messages[messageIndex].author === client.sessionId) {
-//                 const deletedMessage = this.messages.splice(messageIndex, 1)[0];
-//                 this.broadcast("delete-message", { id: deletedMessage.id });
-//             }
-//         });
-//     }
-//
-//     onJoin(client) {
-//         console.log(`${client.sessionId} joined the room`);
-//         client.send("init-messages", this.messages);
-//     }
-//
-//     onLeave(client) {
-//         console.log(`${client.sessionId} left the room`);
-//     }
-//
-//     onDispose() {
-//         console.log("Room disposed");
-//     }
-// }
-//
-// module.exports = { ChatRoom };
+const { Room } = require('colyseus');
+
+class ChatRoom extends Room {
+    onCreate(options) {
+        this.messages = [];
+
+        this.onMessage("message", (client, message) => {
+            const newMessage = { id: `${Date.now()}-${Math.random()}`, author: client.sessionId, ...message };
+            this.messages.push(newMessage);
+            this.broadcast("message", newMessage);
+        });
+
+        this.onMessage("edit-message", (client, updatedMessage) => {
+            const messageIndex = this.messages.findIndex(m => m.id === updatedMessage.id);
+            if (messageIndex !== -1 && this.messages[messageIndex].author === client.sessionId) {
+                this.messages[messageIndex].text = updatedMessage.text;
+                this.broadcast("edit-message", this.messages[messageIndex]);
+            }
+        });
+
+        this.onMessage("delete-message", (client, { id }) => {
+            const messageIndex = this.messages.findIndex(m => m.id === id);
+            if (messageIndex !== -1 && this.messages[messageIndex].author === client.sessionId) {
+                const deletedMessage = this.messages.splice(messageIndex, 1)[0];
+                this.broadcast("delete-message", { id: deletedMessage.id });
+            }
+        });
+    }
+
+    onJoin(client) {
+        console.log(`${client.sessionId} присоединился к комнате`);
+        client.send("init-messages", this.messages);
+    }
+
+    onLeave(client) {
+        console.log(`${client.sessionId} покинул комнату`);
+    }
+
+    onDispose() {
+        console.log("Комната удалена");
+    }
+}
+
+module.exports = { ChatRoom };
--- server.js
+++ server.js
@@ -1,61 +1,14 @@
 const { createServer } = require('http');
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const WebSocket = require('ws');
+const { Server } = require('colyseus');
+const { ChatRoom } = require('./ChatRoom');
 
 const app = express();
 const httpServer = createServer(app);
-const wss = new WebSocket.Server({ server: httpServer });
+const gameServer = new Server({ server: httpServer });
 
-// Замените 'your_secret_key' на ваш реальный ключ
-const secretKey = 'your_secret_key';
+gameServer.define('chat', ChatRoom);
 
-app.use(cors({
-    origin: 'http://localhost:5174', // Укажите ваш фронтенд URL
-    credentials: true,
-}));
-
-app.use(express.json());
-
-app.post('/login', (req, res) => {
-    const { username } = req.body;
-    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-    res.json({ token });
-});
-
-function handleMessage(ws, message) {
-    try {
-        const parsedMessage = JSON.parse(message);
-        console.log(`Received message: ${parsedMessage.text}`);
-
-        // Broadcast the message to all clients
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(parsedMessage));
-            }
-        });
-    } catch (error) {
-        console.error('Failed to parse message:', error);
-    }
-}
-
-wss.on('connection', (ws) => {
-    console.log('New client connected');
-
-    ws.on('message', (message) => {
-        handleMessage(ws, message);
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-
-    ws.on('error', (error) => {
-        console.error('WebSocket error:', error);
-    });
-});
-
-httpServer.listen(2567, () => {
-    console.log("WebSocket server is running on ws://localhost:2567");
+gameServer.listen(2567, () => {
+    console.log('Colyseus сервер запущен на ws://localhost:2567');
 });
